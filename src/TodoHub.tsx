0567e750a9849dffe52838728be07e26962e3982
rogercodes1 <rogerperez.us@gmail.com>
2020-03-02 23:17:24 -0800

    renamed sections and work in progress

--- src/TodoHub.tsx
+++ src/TodoHub.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import "./App.css";
-import { Segment, Header, Icon, Button } from "semantic-ui-react";
+import "./TodoHub.css";
+import {
+  Segment,
+  Header,
+  Icon,
+  Button,
+  Grid,
+  Divider,
+  Form
+} from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 
 interface IHubProps {}
@@ -18,28 +26,23 @@ class TodoHub extends React.Component<IHubProps, IHubState> {
     return (
       <div className="main">
         <Segment className="todo-today">
-          Pellentesque habitant morbi tristique senectus.
-          <Header icon>
-            <Icon name="file pdf outline" />
-            No documents are listed for this customer.
+          <Header as="h2" floated="right">
+            Floated Content
           </Header>
-          <Button primary>Add Document</Button>
-        </Segment>
-        <Segment className="todo-today">
-          Pellentesque habitant morbi tristique senectus.
-          <Header icon>
-            <Icon name="file pdf outline" />
-            No documents are listed for this customer.
-          </Header>
-          <Button primary>Add Document</Button>
-        </Segment>
-        <Segment className="todo-today">
-          Pellentesque habitant morbi tristique senectus.
-          <Header icon>
-            <Icon name="file pdf outline" />
-            No documents are listed for this customer.
-          </Header>
-          <Button primary>Add Document</Button>
+          <Form>
+            <Form.Input
+              icon="user"
+              iconPosition="left"
+              label="Username"
+              placeholder="Username"
+              className="todo-input"
+            />
+
+            <Button content="Login" primary />
+          </Form>
+          <div>
+            <Button content="Sign up" icon="signup" size="big" />
+          </div>
         </Segment>
       </div>
     );
