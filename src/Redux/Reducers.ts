0a581b2f348dcab8e76beb42919b78ba3583a73b
rogercodes1 <rogerperez.us@gmail.com>
2020-03-02 22:04:29 -0800

    updated reducers

--- src/Redux/Reducers.ts
+++ src/Redux/Reducers.ts
@@ -1,48 +1,42 @@
-import { IAppState, actionMethods, viewActionTypes } from './Types';
-import * as types from './Types';
-
+import { IAppState, actionMethods, viewActionTypes } from "./Types";
+import * as types from "./Types";
 
 export function defaultState(): IAppState {
-    return {
-        loading: false,
-        today: "today's date"
-    }
+  return {
+    loading: false,
+    today: "today's date"
+  };
 }
-
 
 export function appState(
-    state = defaultState(),
-    action: actionMethods
+  state = defaultState(),
+  action: actionMethods
 ): IAppState {
-    switch (action.type) {
-        case viewActionTypes.ADD_ERROR:
-            return onShowError(state,action);
-        case viewActionTypes.CANCEL:
-            
-            return onCancel(state);
-    
-        default:
-            return state;
-        }
-}
+  switch (action.type) {
+    case viewActionTypes.ADD_ERROR:
+      return onShowError(state, action);
+    case viewActionTypes.CANCEL:
+      return onCancel(state);
 
+    default:
+      return state;
+  }
+}
 
 const onShowError = (
-    state: IAppState,
-    action: types.IOnShowError
+  state: IAppState,
+  action: types.IOnShowError
 ): IAppState => {
-    return {
-        ...state,
-        error: action.error,
-        loading: false
-    }
-}
+  return {
+    ...state,
+    error: action.error,
+    loading: false
+  };
+};
 
-const onCancel = (
-    state: IAppState
-): IAppState => {
-    return {
-        ...state,
-        loading: false
-    }
-}
+const onCancel = (state: IAppState): IAppState => {
+  return {
+    ...state,
+    loading: false
+  };
+};
